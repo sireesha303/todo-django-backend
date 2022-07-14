f7685f7dacb5a0f77b65f6812fac5647c33d18a0
sireesha <msireesha30031999@gmail.com>
2022-07-14 22:18:22 +0530

    heroku setup modified-static root

--- todo_django_backend/settings.py
+++ todo_django_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a&=min6f9nj%g%6wfgh+!l8wi8yei^y^3_iwwkszw%(!nydcja
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'todoapp-django-backend.herokuapp.com']
 
 
 # Application definition
@@ -128,9 +128,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

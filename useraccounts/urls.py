89fd04d547a8fea1ed195432380d67ccf429d256
sireesha <msireesha30031999@gmail.com>
2022-12-04 21:39:12 +0530

    updated requirements file

--- useraccounts/urls.py
+++ useraccounts/urls.py
@@ -1,16 +1,14 @@
-from django.template.defaulttags import url
+from django.conf.urls import url
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from django.conf.urls import  include
 from .views import *
 
 urlpatterns = [
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     url(r'^account/', include('djoser.urls')),
-
 ]
 

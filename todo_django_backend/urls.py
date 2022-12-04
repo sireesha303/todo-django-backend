d4a3536b000378811ff009fc3c10116b897cec0d
sireesha <msireesha30031999@gmail.com>
2022-12-04 21:56:10 +0530

    static files url added

--- todo_django_backend/urls.py
+++ todo_django_backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -25,3 +27,4 @@ urlpatterns = [
 
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
